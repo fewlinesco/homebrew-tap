e32a0c1c83cc90e855bbc1e8015a1e0791c3e5f3
Julien Leclercq <julien.leclercq@gmx.fr>
2020-09-15 14:19:02 +0200

    fix sha256

--- Formula/fwl_error.rb
+++ Formula/fwl_error.rb
@@ -2,7 +2,7 @@ class FwlError < Formula
   desc "Fewlines error code generator (internal use)"
   homepage "https://github.com/fewlinesco/error-codes-generator"
   url "https://github.com/fewlinesco/error-codes-generator/archive/v1.0.tar.gz"
-  sha "(sha will be replaced by github release)"
+  sha256 "(sha will be replaced by github release)"
 
   def install
     bin.install "fwl-error"
