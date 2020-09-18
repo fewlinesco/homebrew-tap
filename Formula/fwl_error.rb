17ff62cd57a371033d4760ecb0535c8b11c9f2e2
F. Ewlines <31658301+frieda-ewlines@users.noreply.github.com>
2020-09-18 18:01:46 +0200

    fwl_error 1.0.5

--- Formula/fwl_error.rb
+++ Formula/fwl_error.rb
@@ -1,7 +1,7 @@
 class FwlError < Formula
   desc "Fewlines error code generator (internal use)"
   homepage "https://github.com/fewlinesco/error-codes-generator"
-  url "https://github.com/fewlinesco/error-codes-generator/releases/download/v1.0.3/fwl-error-darwin-amd64.tar.gz"
+  url "https://github.com/fewlinesco/error-codes-generator/releases/download/v1.0.5/fwl-error-darwin-amd64.tar.gz"
   sha256 "e0f773240e68068953a2978d820c9305fb7f83c1dd4c6ac0cbf99e61a42e74cf"
 
   def install
